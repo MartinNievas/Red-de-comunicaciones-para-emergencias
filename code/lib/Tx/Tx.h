ce557140ff25f67bd98dd94f9ed250f129e1d0bb
martin nievas <martin.nievas.ar@gmail.com>
2021-05-21 20:47:58 -0300

    Agrega reglas de compilación a la biblioteca externa
      - Esto es en parte para resolver #12

--- code/lib/Tx/Tx.h
+++ code/lib/Tx/Tx.h
@@ -1,9 +1,12 @@
 #include <Bateria.h>
 
+#ifndef TX_H
+  #define TX_X
+
 String gps(){
     return "(146,231,22)";
 }
-  
+
 String hora(){
     return "2:07";
 }
@@ -11,22 +14,24 @@ String hora(){
 String mensaje(){
     switch (random(0,4)) {
       case 0:return("\"ASISTENCIA\"");
-      case 1:return("\"HERIDO\"");   
-      case 2:return("\"CONTROLADO\"");   
-      case 3:return("\"OK\"");   
+      case 1:return("\"HERIDO\"");
+      case 2:return("\"CONTROLADO\"");
+      case 3:return("\"OK\"");
    }
 }
 
 class PaqueteEnviado{
-  
+
   public:
 
   String GPS(){return gps();}
   String Hora(){return hora();}
   float bateria(){
-    Bateria bat; 
+    Bateria bat;
     return bat.Restante();
     }
   String Mensaje(){return mensaje();}
-  
+
 };
+
+#endif
