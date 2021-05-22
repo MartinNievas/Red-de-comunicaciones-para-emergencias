a7b8bd6a52f90e770ca6fffc3523e6dd7d3b03ee
martin nievas <martin.nievas.ar@gmail.com>
2021-05-22 00:50:34 -0300

    Reordena la definición de la clase en el .h

--- code/lib/Bateria/Bateria.cpp
+++ code/lib/Bateria/Bateria.cpp
@@ -1,27 +1,2 @@
 #include <Bateria.h>
 
-class Bateria {
-  private:
-     float carga;
-
-  public:
-
-  Bateria(){
-    carga = nivelADC();  
-  }
-  
-  float nivelADC(){
-    float ADC = 462.5;
-    carga = ADC;
-    return carga;
-  }
-
-  float nivel(){
-    return carga;
-  }
-
-  float Restante (){
-    int maxCarga = 2200;
-    return (carga*100/maxCarga);
-  }    
-};
--- code/lib/Bateria/Bateria.h
+++ code/lib/Bateria/Bateria.h
@@ -1,4 +1,30 @@
 #ifndef BAT_H
   #define BAT_X
 
+class Bateria {
+  private:
+     float carga;
+
+  public:
+
+  Bateria(){
+    carga = nivelADC();
+  }
+
+  float nivelADC(){
+    float ADC = 462.5;
+    carga = ADC;
+    return carga;
+  }
+
+  float nivel(){
+    return carga;
+  }
+
+  float Restante (){
+    int maxCarga = 2200;
+    return (carga*100/maxCarga);
+  }
+};
+
 #endif
