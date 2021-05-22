71314aca9cd272fc3714afe09bb63f1c9026543b
Matias Amaya <matissamaya@gmail.com>
2021-05-21 22:16:59 -0300

    Corrección del formato de librería Rx

--- code/lib/Rx/Rx.h
+++ code/lib/Rx/Rx.h
@@ -1,27 +1,6 @@
-String Auxiliar(String str, int h){
-  int j,i=0,k=0;  
-  char aux[5][15]; 
-  for(j=0;j<str.length();j++){
-       if (str[j]!='-'){
-          aux[i][k]=str[j];
-          k++;
-       }
-       else{   
-          aux[i][k]='\0';     
-          i++;
-          k=0;
-       } 
-    }
-   return aux[h];
-}
+#ifndef RX_H
+  #define RX_X
 
-class PaqueteRecibido{
-  
-  public:
+  String Auxiliar(String,int);
 
-  String ID(String str){return Auxiliar(str,0);}
-  String GPS(String str){return Auxiliar(str,1);}
-  String Hora(String str){return Auxiliar(str,2);}
-  String Bateria(String str){return Auxiliar(str,3);}
-  String Mensaje(String str){return Auxiliar(str,4);}
-};
+#endif
