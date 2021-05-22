b8e50a71ef51812ebc0f96df891d003fcd26e017
martin nievas <martin.nievas.ar@gmail.com>
2021-05-22 00:50:42 -0300

    Reordena la definición de la clase en el .h

--- code/lib/Rx/Rx.cpp
+++ code/lib/Rx/Rx.cpp
@@ -1,29 +1,19 @@
 #include <Rx.h>
 
 String Auxiliar(String str, int h){
-  int j,i=0,k=0;  
-  char aux[5][15]; 
+  int j,i=0,k=0;
+  char aux[5][15];
   for(j=0;j<str.length();j++){
        if (str[j]!='-'){
           aux[i][k]=str[j];
           k++;
        }
-       else{   
-          aux[i][k]='\0';     
+       else{
+          aux[i][k]='\0';
           i++;
           k=0;
-       } 
+       }
     }
    return aux[h];
 }
 
-class PaqueteRecibido{
-  
-  public:
-
-  String ID(String str){return Auxiliar(str,0);}
-  String GPS(String str){return Auxiliar(str,1);}
-  String Hora(String str){return Auxiliar(str,2);}
-  String Bateria(String str){return Auxiliar(str,3);}
-  String Mensaje(String str){return Auxiliar(str,4);}
-};
--- code/lib/Rx/Rx.h
+++ code/lib/Rx/Rx.h
@@ -3,4 +3,15 @@
 
   String Auxiliar(String,int);
 
+class PaqueteRecibido{
+
+  public:
+
+  String ID(String str){return Auxiliar(str,0);}
+  String GPS(String str){return Auxiliar(str,1);}
+  String Hora(String str){return Auxiliar(str,2);}
+  String Bateria(String str){return Auxiliar(str,3);}
+  String Mensaje(String str){return Auxiliar(str,4);}
+};
+
 #endif
