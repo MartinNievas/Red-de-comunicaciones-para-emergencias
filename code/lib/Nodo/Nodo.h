1b4388056f6f7c7ea4ef91e9a771a0a2c83422b3
Matias Amaya <matissamaya@gmail.com>
2021-06-01 17:27:12 -0300

    Dentro de las varias modificaciones, se agrega el objeto Pantalla, el cual imprime en pantalla los datos provenientes del reloj, la batería y la ID del nodo. De manera que, esta pantalla es la visualización del display en estado estacionario (Sin comunicaciones). #15.

--- code/lib/Nodo/Nodo.h
+++ code/lib/Nodo/Nodo.h
@@ -7,23 +7,37 @@
   #define NODO_X
   
   void Transmision(char,String);
-  void Auxiliar(String);
+  int Control(String,String,char);
+  String Mensajes(void);
+  int controlERR(String,String,char);
+  int controlID(String,char);
+  int controlSEP(String);
+  int controlHR(String);
+  int controlBAT(String);
+  int controlMSJ(String);
+  String Barra(void);
   
   class Nodo{
 
     private:
       char NodeID;
-  
+      String Buffer;
+      int Blink=0;
+
     public:
       Nodo(){
         NodeID = '?';
+        Buffer = "?";
       }
   
       int Inicializar(){
         if(!LORA::Inicializar()&&!LCD::Inicializar()){
-           LCD::Escribir("Inicializando...",1);
-           delay (1000);
-           LCD::Limpiar();
+           LCD::Letra(1);
+           LCD::Escribir("Inicializando",0,0);
+           for(int i=0;i<4;i++){
+                delay(400);
+                LCD::Escribir(".",-1,-1);
+           }
            return 0;
         }
         else
@@ -36,17 +50,38 @@
   
       void Enviar(String Mensaje) {
         Transmision(NodeID,Mensaje);
+        Buffer=Mensaje;
       } 
-
-      void Escuchar(void){
-        LORA::Escuchar();
-      }
   
-      void Leer(){
+      int Escuchar(){
         String paqueteRecibido = LORA::Leer();
-        if (paqueteRecibido != "-1"&&paqueteRecibido[0]!=NodeID)Auxiliar(paqueteRecibido);
+        if (paqueteRecibido=="-1") return 1;
+        if (Control(paqueteRecibido,Buffer,NodeID)&&paqueteRecibido[0]!=NodeID){
+          LCD::Limpiar();
+          LCD::Letra(1);
+          LCD::Escribir("ERROR DE TRAMA",0,0);
+          LCD::Escribir("Pidiendo reenvio",0,25);
+          LORA::Enviar("###");
+          LORA::Escuchar();
+          for(int i=0;i<4;i++){
+             delay(400);
+             LCD::Escribir(".",-1,-1);
+          }
+          Escuchar();
+        }
+        return 0;
       }
-  
+
+      void Pantalla(){
+         char nID[2]={NodeID,'\0'};
+         LCD::Limpiar();
+         LCD::Letra(4);
+         LCD::Escribir(Hora(),5,15);
+         LCD::Letra(1);
+         LCD::Escribir(Barra(),56,57); 
+         LCD::Letra(1.5);
+         LCD::Escribir(nID,0,57);          
+      }
   };
 
 #endif
